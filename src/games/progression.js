ebd33c558543944a77d4ec18a91130562737a4df
Alexander Proshlyakov <tashimotor@gmail.com>
2025-11-03 17:37:55 +0300

    remove magic numbers

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,9 +1,14 @@
 const getDescription = () => 'What number is missing in the progression?'
+const MIN_PROGRESSION_LENGTH = 5
+const MAX_PROGRESSION_LENGTH = 10
+const MAX_START = 20
+const MIN_STEP = 1
+const MAX_STEP = 10
 
 const generateRound = () => {
-  const length = Math.floor(Math.random() * 6) + 5
-  const start = Math.floor(Math.random() * 20)
-  const step = Math.floor(Math.random() * 10) + 1
+  const length = Math.floor(Math.random() * (MAX_PROGRESSION_LENGTH - MIN_PROGRESSION_LENGTH + 1)) + MIN_PROGRESSION_LENGTH
+  const start = Math.floor(Math.random() * MAX_START)
+  const step = Math.floor(Math.random() * (MAX_STEP - MIN_STEP + 1)) + MIN_STEP
   const hiddenIndex = Math.floor(Math.random() * length)
   const progression = []
   for (let i = 0; i < length; i += 1) {
